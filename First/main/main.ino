daafe5763d149bf88a294655b254e2d55c1bce28
Zoz606 <87106742+Zoz606@users.noreply.github.com>
2021-10-21 10:06:18 +0200

    new

---
+++ First/main/main.ino
@@ -0,0 +1,11 @@
+int myVar = 5;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(myVar);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
